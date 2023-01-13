ab64010fa352ef52cce541753aad059b9d07fd79
ACC <aderitocaxala10@gmail.com>
2023-01-13 20:47:47 +0100

    validating the fields

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 {/* <p class="error">Can't be blank</p> */}
+function ClientCard(name, number, date, cvc) {
+    this.name = name.value;
+    this.number = number.value;
+    this.isValidNumber = () =>{
+        if(Number(this.number) === NaN && this.number.length > 16 || this.number.length < 16)
+            return false;
+        return true;
+    }
+    this.verifyFields = () =>{
+        if (!this.isValidNumber(number)) {
+            this.setError(number, );
+        }
+    } 
+    this.setError = (field, errorName) =>{
+        field.style.borderColor = 'var(--Red);';
+        field.parentElement.innerHTML += errorName;
+    }
+}
 const btnConfirm = document.querySelector("#btnConfirm");
+btnConfirm.addEventListener("click");
